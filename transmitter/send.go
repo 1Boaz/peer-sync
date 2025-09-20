82e8196f27cc0fb0daf852f2aa3bc28d49883aaf
1Boaz <bizbizdrori@gmail.com>
2025-09-20 12:53:18 +0300

    refactor send function to use structured JSON type for improved readability and maintainability

--- transmitter/send.go
+++ transmitter/send.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 )
 
-func send(filepath string, file string) *http.Response {
-	json := fmt.Sprintf(`{"path":"%s","content":"%s"}`, filepath, file)
-	fmt.Println("Sending:", json)
-	resp, err := http.Post("http://192.168.1.119:8080/", "application/json", strings.NewReader(json))
+type Json struct {
+	Path    string `json:"path"`
+	Content string `json:"content"`
+}
+
+func send(path string, file string) *http.Response {
+	data := Json{Path: path, Content: file}
+
+	json, err := json.Marshal(data)
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("Sending:", string(json))
+	resp, err := http.Post("http://192.168.1.117:8080/", "application/json", strings.NewReader(string(json)))
+	if err != nil {
+		panic(err)
+	}
+
 	return resp
 }
