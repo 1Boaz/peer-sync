6f7e7338aebf0e454c986b72f97e1b28f05f02f0
1Boaz <bizbizdrori@gmail.com>
2025-09-23 15:46:18 +0300

    enhanced main.go to utilize fsnotify for file watching

--- transmitter/go.mod
+++ transmitter/go.mod
@@ -5,4 +5,6 @@ go 1.24.6
 require (
 	github.com/alexflint/go-arg v1.6.0 // indirect
 	github.com/alexflint/go-scalar v1.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 )
--- transmitter/main.go
+++ transmitter/main.go
@@ -1,13 +1,54 @@
 package main
 
-import "sync"
+import (
+	"log"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+)
 
 func main() {
 	conf := getConfig()
-	var wg sync.WaitGroup
-	for _, path := range conf.Paths {
-		wg.Add(1)
-		go read(path, &wg, conf.Url)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return
 	}
-	wg.Wait()
+
+	defer watcher.Close()
+
+	go listen(conf, watcher)
+
+	for _, path := range conf.Paths {
+		err = watcher.Add(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	<-make(chan struct{})
+}
+
+func listen(conf Config, watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			log.Println("event:", event)
+			if event.Has(fsnotify.Write) {
+				var wg sync.WaitGroup
+				for _, path := range conf.Paths {
+					wg.Add(1)
+					go read(path, &wg, conf.Url)
+				}
+				wg.Wait()
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
 }
