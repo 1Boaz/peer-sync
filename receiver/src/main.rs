230c4fa1e32ad286864cfdfe42319a85af360b31
1Boaz <148672856+1Boaz@users.noreply.github.com>
2026-02-02 12:49:12 +0000

    Refactor receiver to use Axum framework and remove Actix dependencies
    - Replaced Actix-web with Axum in Cargo.toml and main.rs.
    - Removed command-line argument parsing using Clap and associated struct.
    - Deleted error handling module and middleware for authorization.
    - Removed file handling endpoints (save and delete) and their implementations.
    - Cleaned up unnecessary imports and code related to Actix.
    - Updated main function to serve a simple Axum application.
    - Removed transmitter configuration and file handling logic.

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,65 +1,15 @@
-mod error;
-mod save;
-mod args;
-mod middleware;
-mod remove;
-
-use std::io;
-use std::io::Error;
-use actix_web::{web, App, HttpServer};
-use actix_web::middleware::from_fn;
-use clap::Parser;
-use local_ip_address::local_ip;
-use tracing::{info, Level};
-use tracing_subscriber::FmtSubscriber;
+use axum::{Router, routing::get};
 
 
-/// The main entry point of the application.
-///
-/// This function parses the command-line arguments, retrieves the local IP address,
-/// initializes tracing, and starts the Actix Web server.
-///
-/// The server is configured to use the provided passkey for authorization,
-/// and to listen on the provided IP and port.
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    let args = args::ReceiverArgs::parse();
-    let passkey = args.key;
-    let ip = get_ip()?;
-
-    // Initialize tracing
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::INFO)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)
-        .expect("setting default subscriber failed");
-
-    info!("Starting server at http://{}:{}", ip, args.port);
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::PayloadConfig::new(80_000_000))
-            .app_data(web::Data::new(passkey.clone()))
-            .wrap(tracing_actix_web::TracingLogger::default())
-            .wrap(from_fn(middleware::validate))
-            .service(save::save)
-            .service(remove::delete)
-    })
-        .bind((ip, args.port))?
-        .run()
-        .await
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(axum_is_solid));
+        
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
-/// Returns the local IP address as a string.
-///
-/// # Errors
-///
-/// * Returns an error if the local IP address cannot be determined.
 
-fn get_ip() -> Result<String, Error> {
-    local_ip()
-        .map(|ip| ip.to_string())
-        .map_err(|e| {
-            tracing::error!("Failed to get local IP: {}", e);
-            Error::new(io::ErrorKind::Other, format!("Failed to get local IP: {}", e))
-        })
+async fn axum_is_solid() -> &'static str {
+    "dsf"
 }
