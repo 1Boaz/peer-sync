bdcff8b039d3a1d3a39aed8c7e7a433ddfbf30a7
1Boaz <bizbizdrori@gmail.com>
2026-03-07 18:24:45 +0200

    Created a new structure for full rust with a shared lib

--- receiver/src/args.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-pub struct ReceiverArgs {
-    #[arg(long, short, default_value_t = 31415)]
-    /// Port to host the receiver on
-    pub port: u16
-}
