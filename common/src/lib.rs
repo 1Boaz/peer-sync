0c1dabd46080b7c331d54e2b2f3f6e6ef305d127
1Boaz <bizbizdrori@gmail.com>
2026-03-08 15:09:39 +0200

    Created the protocol for tcp stream

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use rkyv::{rancor::Error, Archive, Deserialize, Serialize};
+use rkyv::util::AlignedVec;
+
+// The enum that holds the protocol
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[rkyv(compare(PartialEq), derive(Debug))]
+pub enum SyncMessage {
+    // Indicates new file
+    NewFile {
+        path: String,
+        perm: u32
+    },
+
+    // Chunk from a file
+    Chunk(Vec<u8>),
+
+    // End of the file
+    EndFile
+}
+
+// Helper function to serialize
+pub fn serialize(sync_message: &SyncMessage) -> Result<AlignedVec, Error> {
+     rkyv::to_bytes::<Error>(sync_message)
+}
+
+// Helper function to access(look at the data without changing it)
+pub fn access_message(serialized: &AlignedVec) -> Result<&ArchivedSyncMessage, Error> {
+    rkyv::access::<ArchivedSyncMessage, Error>(serialized)
 }
 
 #[cfg(test)]
@@ -7,8 +33,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn ser_access_newfile_test() {
+        let demo_sync_message: SyncMessage = SyncMessage::NewFile {
+            path: "/hi".to_string(),
+            perm: 5
+        };
+        let serialized = serialize(&demo_sync_message).unwrap();
+        let accessed = access_message(&serialized).unwrap();
+        assert_eq!(*accessed, demo_sync_message);
+    }
+
+    #[test]
+    fn ser_access_chunk_test() {
+        let demo_sync_message: SyncMessage = SyncMessage::Chunk(vec![1, 2, 5, 6, 7, 2, 8, 1]);
+        let serialized = serialize(&demo_sync_message).unwrap();
+        let accessed = access_message(&serialized).unwrap();
+        assert_eq!(*accessed, demo_sync_message);
+    }
+
+    #[test]
+    fn ser_access_endfile_test() {
+        let demo_sync_message: SyncMessage = SyncMessage::EndFile;
+        let serialized = serialize(&demo_sync_message).unwrap();
+        let accessed = access_message(&serialized).unwrap();
+        println!("{:?}", accessed);
+        assert_eq!(*accessed, demo_sync_message);
     }
 }
